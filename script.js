31438151d82b4d8420adcbe4223b095aaa268696
pmallol <paulamallol@gmail.com>
2024-08-12 20:25:54 -0300

    Add index page and styles and script

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const steps = document.querySelectorAll('.step');
+  steps.forEach((step) => {
+    step.addEventListener('click', function () {
+      document
+        .querySelectorAll('.step')
+        .forEach((s) => s.classList.remove('active'));
+
+      document
+        .querySelectorAll('.step-content')
+        .forEach((content) => (content.style.display = 'none'));
+
+      this.classList.add('active');
+
+      const stepNumber = this.getAttribute('data-step');
+      document.getElementById(`step${stepNumber}-content`).style.display =
+        'block';
+
+      document.getElementById('mainImage').src = `step${stepNumber}.jpg`;
+    });
+  });
+});
