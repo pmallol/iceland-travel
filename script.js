940aafbef8a0c0c8ef3ef1276f33b5dc4d7b8e03
pmallol <paulamallol@gmail.com>
2024-08-12 22:28:25 -0300

    Improve step display animation on click

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+function activateStep(activeStep) {
+  const stepNumber = activeStep.getAttribute('data-step');
+  const stepNumberContent = document.getElementById(
+    `step${stepNumber}-content`
+  );
+  stepNumberContent.style.visibility = 'visible';
+  stepNumberContent.style.opacity = '1';
+  stepNumberContent.style.height = 'auto';
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   const activeStep = document.querySelector('.step.active');
   if (activeStep) {
-    const stepNumber = activeStep.getAttribute('data-step');
-    document.getElementById(`step${stepNumber}-content`).style.display =
-    'block';
+    activateStep(activeStep);
   }
-  
+
   const steps = document.querySelectorAll('.step');
   steps.forEach((step) => {
     step.addEventListener('click', function () {
@@ -13,19 +21,18 @@ document.addEventListener('DOMContentLoaded', function () {
         .querySelectorAll('.step')
         .forEach((s) => s.classList.remove('active'));
 
-      document
-        .querySelectorAll('.step-content')
-        .forEach((content) => (content.style.display = 'none'));
+      document.querySelectorAll('.step-content').forEach((content) => {
+        content.style.visibility = 'hidden';
+        content.style.opacity = '0';
+        content.style.height = '0';
+      });
 
       this.classList.add('active');
-
-      const stepNumber = this.getAttribute('data-step');
-      document.getElementById(`step${stepNumber}-content`).style.display =
-        'block';
+      activateStep(this);
 
       document.getElementById(
         'mainImage'
-      ).src = `assets/images/step${stepNumber}.jpg`;
+      ).src = `assets/images/step${this.getAttribute('data-step')}.jpg`;
     });
   });
 });
